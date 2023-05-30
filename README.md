2ad638a2d720a652574d862eb92d4383fa1a1ed9
UrielCh <uriel.chemouni@gmail.com>
2023-05-30 18:21:39 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Simple Line Icons icons for deno / Preact
+
+**License** [MIT](https://opensource.org/licenses/MIT)
+
+**Project** [https://thesabbir.github.io/simple-line-icons/](https://thesabbir.github.io/simple-line-icons/)
+
+[See available icons here](https://react-icons.github.io/react-icons/icons?name=sl)
+
+## import_map.json
+
+For a transparent usage:
+
+```json
+{
+  "imports": {
+    "preact": "https://esm.sh/preact@10.15.1",
+    "preact/": "https://esm.sh/preact@10.15.1/",
+    "react-icons/sl": "https://deno.land/x/react_icons_sl@1.0.0//mod.ts",
+  }
+}
+```
+
+## Direct import sample
+
+`import { SlActionRedo } from "https://deno.land/x/react_icons_sl@1.0.0/mod.ts"`
+
+## import_map import sample
+
+`import { SlActionRedo } from "react-icons/sl"`
+
---
+++ mod.ts
@@ -0,0 +1,224 @@
+// Copyright 2016-2022 the Simple Line Icons authors. All rights reserved. MIT (https://opensource.org/licenses/MIT).
+/**
+ * # Simple Line Icons icons for deno / Preact
+ * 
+ * **License** [MIT](https://opensource.org/licenses/MIT)
+ * 
+ * **Project** [https://thesabbir.github.io/simple-line-icons/](https://thesabbir.github.io/simple-line-icons/)
+ * 
+ * [See available icons here](https://react-icons.github.io/react-icons/icons?name=sl)
+ * 
+ * ## import_map.json
+ * 
+ * For a transparent usage:
+ * 
+ * ```json
+ * {
+ *   "imports": {
+ *     "preact": "https://esm.sh/preact@10.15.1",
+ *     "preact/": "https://esm.sh/preact@10.15.1/",
+ *     "react-icons/sl": "https://deno.land/x/react_icons_sl@1.0.0//mod.ts",
+ *   }
+ * }
+ * ```
+ * 
+ * ## Direct import sample
+ * 
+ * `import { SlActionRedo } from "https://deno.land/x/react_icons_sl@1.0.0/mod.ts"`
+ * 
+ * ## import_map import sample
+ * 
+ * `import { SlActionRedo } from "react-icons/sl"`
+ * 
+ * @module
+ */
+
+export { SlActionRedo } from './ico/SlActionRedo.ts';
+export { SlActionUndo } from './ico/SlActionUndo.ts';
+export { SlAnchor } from './ico/SlAnchor.ts';
+export { SlArrowDownCircle } from './ico/SlArrowDownCircle.ts';
+export { SlArrowDown } from './ico/SlArrowDown.ts';
+export { SlArrowLeftCircle } from './ico/SlArrowLeftCircle.ts';
+export { SlArrowLeft } from './ico/SlArrowLeft.ts';
+export { SlArrowRightCircle } from './ico/SlArrowRightCircle.ts';
+export { SlArrowRight } from './ico/SlArrowRight.ts';
+export { SlArrowUpCircle } from './ico/SlArrowUpCircle.ts';
+export { SlArrowUp } from './ico/SlArrowUp.ts';
+export { SlBadge } from './ico/SlBadge.ts';
+export { SlBag } from './ico/SlBag.ts';
+export { SlBan } from './ico/SlBan.ts';
+export { SlBasketLoaded } from './ico/SlBasketLoaded.ts';
+export { SlBasket } from './ico/SlBasket.ts';
+export { SlBell } from './ico/SlBell.ts';
+export { SlBookOpen } from './ico/SlBookOpen.ts';
+export { SlBriefcase } from './ico/SlBriefcase.ts';
+export { SlBubble } from './ico/SlBubble.ts';
+export { SlBubbles } from './ico/SlBubbles.ts';
+export { SlBulb } from './ico/SlBulb.ts';
+export { SlCalculator } from './ico/SlCalculator.ts';
+export { SlCalender } from './ico/SlCalender.ts';
+export { SlCallEnd } from './ico/SlCallEnd.ts';
+export { SlCallIn } from './ico/SlCallIn.ts';
+export { SlCallOut } from './ico/SlCallOut.ts';
+export { SlCamera } from './ico/SlCamera.ts';
+export { SlCamrecorder } from './ico/SlCamrecorder.ts';
+export { SlChart } from './ico/SlChart.ts';
+export { SlCheck } from './ico/SlCheck.ts';
+export { SlChemistry } from './ico/SlChemistry.ts';
+export { SlClock } from './ico/SlClock.ts';
+export { SlClose } from './ico/SlClose.ts';
+export { SlCloudDownload } from './ico/SlCloudDownload.ts';
+export { SlCloudUpload } from './ico/SlCloudUpload.ts';
+export { SlCompass } from './ico/SlCompass.ts';
+export { SlControlEnd } from './ico/SlControlEnd.ts';
+export { SlControlForward } from './ico/SlControlForward.ts';
+export { SlControlPause } from './ico/SlControlPause.ts';
+export { SlControlPlay } from './ico/SlControlPlay.ts';
+export { SlControlRewind } from './ico/SlControlRewind.ts';
+export { SlControlStart } from './ico/SlControlStart.ts';
+export { SlCreditCard } from './ico/SlCreditCard.ts';
+export { SlCrop } from './ico/SlCrop.ts';
+export { SlCup } from './ico/SlCup.ts';
+export { SlCursorMove } from './ico/SlCursorMove.ts';
+export { SlCursor } from './ico/SlCursor.ts';
+export { SlDiamond } from './ico/SlDiamond.ts';
+export { SlDirection } from './ico/SlDirection.ts';
+export { SlDirections } from './ico/SlDirections.ts';
+export { SlDisc } from './ico/SlDisc.ts';
+export { SlDislike } from './ico/SlDislike.ts';
+export { SlDoc } from './ico/SlDoc.ts';
+export { SlDocs } from './ico/SlDocs.ts';
+export { SlDrawer } from './ico/SlDrawer.ts';
+export { SlDrop } from './ico/SlDrop.ts';
+export { SlEarphonesAlt } from './ico/SlEarphonesAlt.ts';
+export { SlEarphones } from './ico/SlEarphones.ts';
+export { SlEmotsmile } from './ico/SlEmotsmile.ts';
+export { SlEnergy } from './ico/SlEnergy.ts';
+export { SlEnvelopeOpen } from './ico/SlEnvelopeOpen.ts';
+export { SlEnvolopeLetter } from './ico/SlEnvolopeLetter.ts';
+export { SlEnvolope } from './ico/SlEnvolope.ts';
+export { SlEqualizer } from './ico/SlEqualizer.ts';
+export { SlEvent } from './ico/SlEvent.ts';
+export { SlExclamation } from './ico/SlExclamation.ts';
+export { SlEye } from './ico/SlEye.ts';
+export { SlEyeglass } from './ico/SlEyeglass.ts';
+export { SlFeed } from './ico/SlFeed.ts';
+export { SlFilm } from './ico/SlFilm.ts';
+export { SlFire } from './ico/SlFire.ts';
+export { SlFlag } from './ico/SlFlag.ts';
+export { SlFolderAlt } from './ico/SlFolderAlt.ts';
+export { SlFolder } from './ico/SlFolder.ts';
+export { SlFrame } from './ico/SlFrame.ts';
+export { SlGameController } from './ico/SlGameController.ts';
+export { SlGhost } from './ico/SlGhost.ts';
+export { SlGlobeAlt } from './ico/SlGlobeAlt.ts';
+export { SlGlobe } from './ico/SlGlobe.ts';
+export { SlGraduation } from './ico/SlGraduation.ts';
+export { SlGraph } from './ico/SlGraph.ts';
+export { SlGrid } from './ico/SlGrid.ts';
+export { SlHandbag } from './ico/SlHandbag.ts';
+export { SlHeart } from './ico/SlHeart.ts';
+export { SlHome } from './ico/SlHome.ts';
+export { SlHourglass } from './ico/SlHourglass.ts';
+export { SlInfo } from './ico/SlInfo.ts';
+export { SlKey } from './ico/SlKey.ts';
+export { SlLayers } from './ico/SlLayers.ts';
+export { SlLike } from './ico/SlLike.ts';
+export { SlLink } from './ico/SlLink.ts';
+export { SlList } from './ico/SlList.ts';
+export { SlLocationPin } from './ico/SlLocationPin.ts';
+export { SlLockOpen } from './ico/SlLockOpen.ts';
+export { SlLock } from './ico/SlLock.ts';
+export { SlLogin } from './ico/SlLogin.ts';
+export { SlLogout } from './ico/SlLogout.ts';
+export { SlLoop } from './ico/SlLoop.ts';
+export { SlMagicWand } from './ico/SlMagicWand.ts';
+export { SlMagnet } from './ico/SlMagnet.ts';
+export { SlMagnifierAdd } from './ico/SlMagnifierAdd.ts';
+export { SlMagnifierRemove } from './ico/SlMagnifierRemove.ts';
+export { SlMagnifier } from './ico/SlMagnifier.ts';
+export { SlMap } from './ico/SlMap.ts';
+export { SlMenu } from './ico/SlMenu.ts';
+export { SlMicrophone } from './ico/SlMicrophone.ts';
+export { SlMinus } from './ico/SlMinus.ts';
+export { SlMouse } from './ico/SlMouse.ts';
+export { SlMusicToneAlt } from './ico/SlMusicToneAlt.ts';
+export { SlMusicTone } from './ico/SlMusicTone.ts';
+export { SlMustache } from './ico/SlMustache.ts';
+export { SlNote } from './ico/SlNote.ts';
+export { SlNotebook } from './ico/SlNotebook.ts';
+export { SlOptionsVertical } from './ico/SlOptionsVertical.ts';
+export { SlOptions } from './ico/SlOptions.ts';
+export { SlOrganization } from './ico/SlOrganization.ts';
+export { SlPaperClip } from './ico/SlPaperClip.ts';
+export { SlPaperPlane } from './ico/SlPaperPlane.ts';
+export { SlPaypal } from './ico/SlPaypal.ts';
+export { SlPencil } from './ico/SlPencil.ts';
+export { SlPeople } from './ico/SlPeople.ts';
+export { SlPhone } from './ico/SlPhone.ts';
+export { SlPicture } from './ico/SlPicture.ts';
+export { SlPieChart } from './ico/SlPieChart.ts';
+export { SlPin } from './ico/SlPin.ts';
+export { SlPlane } from './ico/SlPlane.ts';
+export { SlPlaylist } from './ico/SlPlaylist.ts';
+export { SlPlus } from './ico/SlPlus.ts';
+export { SlPower } from './ico/SlPower.ts';
+export { SlPresent } from './ico/SlPresent.ts';
+export { SlPrinter } from './ico/SlPrinter.ts';
+export { SlPuzzle } from './ico/SlPuzzle.ts';
+export { SlQuestion } from './ico/SlQuestion.ts';
+export { SlRefresh } from './ico/SlRefresh.ts';
+export { SlReload } from './ico/SlReload.ts';
+export { SlRocket } from './ico/SlRocket.ts';
+export { SlScreenDesktop } from './ico/SlScreenDesktop.ts';
+export { SlScreenSmartphone } from './ico/SlScreenSmartphone.ts';
+export { SlScreenTablet } from './ico/SlScreenTablet.ts';
+export { SlSettings } from './ico/SlSettings.ts';
+export { SlShareAlt } from './ico/SlShareAlt.ts';
+export { SlShare } from './ico/SlShare.ts';
+export { SlShield } from './ico/SlShield.ts';
+export { SlShuffle } from './ico/SlShuffle.ts';
+export { SlSizeActual } from './ico/SlSizeActual.ts';
+export { SlSizeFullscreen } from './ico/SlSizeFullscreen.ts';
+export { SlSocialBehance } from './ico/SlSocialBehance.ts';
+export { SlSocialDribbble } from './ico/SlSocialDribbble.ts';
+export { SlSocialDropbox } from './ico/SlSocialDropbox.ts';
+export { SlSocialFacebook } from './ico/SlSocialFacebook.ts';
+export { SlSocialFoursqare } from './ico/SlSocialFoursqare.ts';
+export { SlSocialGithub } from './ico/SlSocialGithub.ts';
+export { SlSocialGoogle } from './ico/SlSocialGoogle.ts';
+export { SlSocialInstagram } from './ico/SlSocialInstagram.ts';
+export { SlSocialLinkedin } from './ico/SlSocialLinkedin.ts';
+export { SlSocialPintarest } from './ico/SlSocialPintarest.ts';
+export { SlSocialReddit } from './ico/SlSocialReddit.ts';
+export { SlSocialSkype } from './ico/SlSocialSkype.ts';
+export { SlSocialSoundcloud } from './ico/SlSocialSoundcloud.ts';
+export { SlSocialSpotify } from './ico/SlSocialSpotify.ts';
+export { SlSocialSteam } from './ico/SlSocialSteam.ts';
+export { SlSocialStumbleupon } from './ico/SlSocialStumbleupon.ts';
+export { SlSocialTumblr } from './ico/SlSocialTumblr.ts';
+export { SlSocialTwitter } from './ico/SlSocialTwitter.ts';
+export { SlSocialVkontakte } from './ico/SlSocialVkontakte.ts';
+export { SlSocialYoutube } from './ico/SlSocialYoutube.ts';
+export { SlSpeech } from './ico/SlSpeech.ts';
+export { SlSpeedometer } from './ico/SlSpeedometer.ts';
+export { SlStar } from './ico/SlStar.ts';
+export { SlSupport } from './ico/SlSupport.ts';
+export { SlSymbleFemale } from './ico/SlSymbleFemale.ts';
+export { SlSymbolMale } from './ico/SlSymbolMale.ts';
+export { SlTag } from './ico/SlTag.ts';
+export { SlTarget } from './ico/SlTarget.ts';
+export { SlTrash } from './ico/SlTrash.ts';
+export { SlTrophy } from './ico/SlTrophy.ts';
+export { SlUmbrella } from './ico/SlUmbrella.ts';
+export { SlUserFemale } from './ico/SlUserFemale.ts';
+export { SlUserFollow } from './ico/SlUserFollow.ts';
+export { SlUserFollowing } from './ico/SlUserFollowing.ts';
+export { SlUserUnfollow } from './ico/SlUserUnfollow.ts';
+export { SlUser } from './ico/SlUser.ts';
+export { SlVector } from './ico/SlVector.ts';
+export { SlVolume1 } from './ico/SlVolume1.ts';
+export { SlVolume2 } from './ico/SlVolume2.ts';
+export { SlVolumeOff } from './ico/SlVolumeOff.ts';
+export { SlWallet } from './ico/SlWallet.ts';
+export { SlWrench } from './ico/SlWrench.ts';
