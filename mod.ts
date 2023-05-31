209ef3c8db03d28d4259ab72810d3fdc75501d42
urielch <uriel.chemouni@gmail.com>
2023-05-31 18:09:32 +0300

    fix dual @

--- mod.ts
+++ mod.ts
@@ -17,8 +17,8 @@
  *   "imports": {
  *     "preact":  "https://esm.sh/preact@10.15.1",
  *     "preact/": "https://esm.sh/preact@10.15.1/",
- *     "react-icons/sl":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-sl@1.0.4/mod.ts",
- *     "react-icons/sl/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-sl@1.0.4/ico/",
+ *     "react-icons/sl":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-sl@1.0.5/mod.ts",
+ *     "react-icons/sl/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-sl@1.0.5/ico/",
  *   }
  * }
  * ```
@@ -26,7 +26,7 @@
  * ## Direct import sample
  * 
  * ```ts
- * import { SlActionRedo } from "https://deno.land/x/react_icons_sl@1.0.4/mod.ts"
+ * import { SlActionRedo } from "https://deno.land/x/react_icons_sl@1.0.5/mod.ts"
  * ```
  * 
  * ## import_map import sample
