aeed749d929bf6b44d1c7c6b16f157f1e1d5698a
urielch <uriel.chemouni@gmail.com>
2024-07-18 10:53:39 +0200

    update doc

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 [See available icons here](https://react-icons.deno.dev/sl)
 
-## install packahe
+## install the module
 
 ```bash
 deno add @preact-icons/sl
@@ -17,36 +17,15 @@ pnpm dlx jsr add @preact-icons/sl
 bunx jsr add @preact-icons/sl
 ```
 
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
-
-# Simple Line Icons icons for preact
-
-[![JSR](https://jsr.io/badges/@preact-icons/sl)](https://jsr.io/@preact-icons/sl)
-
-**License** [MIT](https://opensource.org/licenses/MIT)
-
-**Project** [https://thesabbir.github.io/simple-line-icons/](https://thesabbir.github.io/simple-line-icons/)
-
-[See available icons here](https://react-icons.deno.dev/sl)
-
-## install packahe
-
-```bash
-deno add @preact-icons/sl
-dnpx jsr add @preact-icons/sl
-pnpm dlx jsr add @preact-icons/sl
-bunx jsr add @preact-icons/sl
+You may need to update your preact mapping to avoid mixing JSR and http import:
+Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+```json
+{
+ "preact": "npm:preact@10.22.1",
+ "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ "preact/hooks": "npm:preact@10.22.1/hooks",
+}
 ```
-
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
 
 ## import an icon from all icons
 
@@ -65,4 +44,3 @@ or using default export
 ```ts
 import SlActionRedo from "react-icons/sl/SlActionRedo.ts"
 ```
-
--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/sl)
  * 
- * ## install packahe
+ * ## install the module
  * 
  * ```bash
  * deno add @preact-icons/sl
@@ -19,36 +19,15 @@
  * bunx jsr add @preact-icons/sl
  * ```
  * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
- * 
- * # Simple Line Icons icons for preact
- * 
- * [![JSR](https://jsr.io/badges/@preact-icons/sl)](https://jsr.io/@preact-icons/sl)
- * 
- * **License** [MIT](https://opensource.org/licenses/MIT)
- * 
- * **Project** [https://thesabbir.github.io/simple-line-icons/](https://thesabbir.github.io/simple-line-icons/)
- * 
- * [See available icons here](https://react-icons.deno.dev/sl)
- * 
- * ## install packahe
- * 
- * ```bash
- * deno add @preact-icons/sl
- * dnpx jsr add @preact-icons/sl
- * pnpm dlx jsr add @preact-icons/sl
- * bunx jsr add @preact-icons/sl
+ * You may need to update your preact mapping to avoid mixing JSR and http import:
+ * Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+ * ```json
+ * {
+ *  "preact": "npm:preact@10.22.1",
+ *  "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  "preact/hooks": "npm:preact@10.22.1/hooks",
+ * }
  * ```
- * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
  * 
  * ## import an icon from all icons
  * 
@@ -67,8 +46,8 @@
  * ```ts
  * import SlActionRedo from "react-icons/sl/SlActionRedo.ts"
  * ```
- * 
  * @module
+ * 
  */
 
 import { GenIcon } from "./deps.ts";
